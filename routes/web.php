b6af7d04e77eb9248e0f7b827e85f68380a06724
bemtorres <benja.mora.torres@gmail.com>
2022-07-12 01:34:04 -0400

    update

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ Route::get('/', function () {
 });
 
 
-
 Route::get('posts','PostController@index')->name('posts.index');
 
 Route::get('posts/create','PostController@create')->name('posts.create');
